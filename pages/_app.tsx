f8bce37045b3baa9b7e224d3c4938dbb66323db3
Aviad Mizrachi <aviadm@gmail.com>
2021-12-09 17:43:48 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { FronteggProvider } from '@frontegg/nextjs';
+
+const contextOptions = {
+  baseUrl: 'https://samples-demo.frontegg.com',
+  clientId: '2e53360e-517e-4c38-a040-ba0e8639f2c7'
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <FronteggProvider contextOptions={contextOptions}>
+    <Component {...pageProps}/>
+  </FronteggProvider>
 }
 
 export default MyApp
+
+
